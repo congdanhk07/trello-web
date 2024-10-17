dfd66393bb8355d6f75a4f3014e7659f0bb29fb0
Cong Danh <danh.tran@dision.tech>
2024-10-17 17:54:46 +0700

    update: config color, theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { Button } from '@mui/material'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
+      <Typography variant='h2' color='primary.main'>
+        Demo Test
+      </Typography>
       <Button variant='contained'>Hello world</Button>
     </>
   )
