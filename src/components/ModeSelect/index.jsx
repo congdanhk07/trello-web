91a7ff05b02a0fb50e1b705559b59ee0b96095a0
Cong Danh <danh.tran@dision.tech>
2024-10-18 16:57:40 +0700

    feat: structure folder, config main layout

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ModeSelect() {
+  return <div>ModeSelect</div>
+}
+
+export default ModeSelect
---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,5 @@
+const BoardDetail = () => {
+  return <div>Detail</div>
+}
+
+export default BoardDetail
