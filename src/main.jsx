9daaeb9541577a061c3060f38153e29eeb428da5
Cong Danh <danh.tran@dision.tech>
2024-10-21 10:40:56 +0700

    feat: update absolute path

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
-import App from './App'
-import theme from './theme'
+import App from '~/App'
+import theme from '~/theme'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,5 +1,5 @@
 import Container from '@mui/material/Container'
-import AppBar from '../../components/Appbar'
+import AppBar from '~/components/Appbar'
 import BoardBar from './BoardBar'
 import BoardContent from './BoardContent'
 
