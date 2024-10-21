d10d873666c06d94c06748ad95f644de9d08c8ca
Cong Danh <danh.tran@dision.tech>
2024-10-21 10:15:47 +0700

    update: refector structure cocde

--- src/components/ModeSelect/index.jsx
+++ src/components/ModeSelect/index.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import DarkModeOutlined from '@mui/icons-material/DarkModeOutlined'
+import LightMode from '@mui/icons-material/LightMode'
+import SettingsBrightnessOutlined from '@mui/icons-material/SettingsBrightnessOutlined'
+import { useColorScheme } from '@mui/material'
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
 
 function ModeSelect() {
-  return <div>ModeSelect</div>
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+      <InputLabel id='label-select-dark-light-mode'>Mode</InputLabel>
+      <Select
+        labelId='label-select-dark-light-mode'
+        id='select-dark-light-mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <LightMode fontSize='small' />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <DarkModeOutlined fontSize='small' />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 1
+            }}
+          >
+            <SettingsBrightnessOutlined fontSize='small' />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
 }
 
 export default ModeSelect
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,5 +1,16 @@
-const BoardDetail = () => {
-  return <div>Detail</div>
+import Container from '@mui/material/Container'
+import AppBar from '../../components/Appbar'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+
+const Board = () => {
+  return (
+    <Container maxWidth={false} disableGutters sx={{ height: '100vh' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
 }
 
-export default BoardDetail
+export default Board
