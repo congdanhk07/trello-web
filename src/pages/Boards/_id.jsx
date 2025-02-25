348605a0530cd5f563910bb19fe216060f6130d6
congdanhk07 <congdanhk07@gmail.com>
2025-02-25 09:59:03 +0700

    update: handle drag and drop to empty column

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -8,13 +8,25 @@ import {
 import AppBar from '~/components/Appbar/Appbar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
+import { generatePlaceholderCard } from '~/utils/formatter'
+import isEmpty from 'lodash/isEmpty'
 const Board = () => {
   const [board, setBoard] = useState(null)
   useEffect(() => {
     // TODO: Tạm thời hard để tập trung vào function, theo chuẩn là sẽ dủng react-router-dom để lấy boardId từ URL
     const boardId = '67b973d1c0173810677f470c'
-    fetchBoardDetailsAPI(boardId).then((data) => {
-      setBoard(data)
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      // Khi refresh web thì cần kiểm tra:
+      // Xử lý vấn đề kéo thả vào một column rỗng
+      // Tạo một card rỗng cho column khi getBoard mà cards ko có item
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceholderCard(column)]
+          column.cardOrderIds = [generatePlaceholderCard(column)._id]
+        }
+      })
+      console.log('board', board)
+      setBoard(board)
     })
     return () => {}
   }, [])
@@ -26,9 +38,14 @@ const Board = () => {
       boardId: board._id
     })
 
+    //Khi tạo column mới thì nó chưa có card nên cũng phải add một placeholder card vào để có thể kéo thả
+    createdColumn.cards = [generatePlaceholderCard(createdColumn)]
+    createdColumn.cardOrderIds = [generatePlaceholderCard(createdColumn)._id]
+
     // Cập nhật lại state board
     // Chúng ta đang tự cập nhật lại dữ liệu render phía FE thay vì gọi lại fetchDetails
     // Điều này phụ thuộc vào spec của dự án có BE hỗ trợ trả về toàn bộ Board dù đây là đang call API create column hay card
+
     const newBoard = { ...board }
     newBoard.columns.push(createdColumn)
     newBoard.columnOrderIds.push(createdColumn._id)
--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -3,6 +3,8 @@ export const capitalizeFirstLetter = (val) => {
   return `${val.charAt(0).toUpperCase()}${val.slice(1)}`
 }
 
+// Generate placeholder card for column empty
+// Khi Column bị rỗng thì không thể kéo card vào trong nên chúng ta tạo ra một card ẩn để mặc định luôn có 1 card thuộc trong column
 export const generatePlaceholderCard = (column) => {
   return {
     _id: `column-id-${column._id}-placeholder-card`,
