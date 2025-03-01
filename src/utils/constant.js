a41949ae6730e13659b9928eeecc3e192c2a3579
Trần Công Danh <congdanhk07@gmail.com>
2025-03-01 23:09:41 +0700

    update: add process env to deploy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,9 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-y3oz.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-y3oz.onrender.com'
+}
+
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
   }
+
   // base: './'
 })
